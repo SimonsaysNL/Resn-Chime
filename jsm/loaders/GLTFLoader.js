26030061dd19e8195246a1da0c81f86a081ace20
Simon <kissingsimon@gmail.com>
2021-04-13 02:58:18 +0200

    Add files via upload

--- jsm/loaders/GLTFLoader.js
+++ jsm/loaders/GLTFLoader.js
@@ -61,7 +61,7 @@ import {
 	Vector3,
 	VectorKeyframeTrack,
 	sRGBEncoding
-} from '../../../build/three.module.js';
+} from 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js';
 
 var GLTFLoader = ( function () {
 
